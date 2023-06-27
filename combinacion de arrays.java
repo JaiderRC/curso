05ac4d1a50afa8902f8fb55f408191e237578ba4
jaider <jaiderricardocf@ufps.edu.co>
2023-06-27 15:04:19 -0500

    correcion

--- combinacion de arrays.java
+++ combinacion de arrays.java
@@ -1,39 +1,36 @@
-import java.io.FileInputStream;
-import java.util.*;
+import javax.swing.*;
 
-public class tarea1 {
-    public static void comparar(String[] comparar){
-        int contador=0;
-        for (int i = 0; i <comparar.length-1; i++) {
-            for (int in = 0; in < comparar.length-1-i; in++) {
-            if (comparar [in+1].compareTo(comparar[in]) <0){
-String xd=comparar[in];
-comparar[in]=comparar[in+1];
-comparar[in+1]=xd;
-
-                }
-            }
-contador++;
-
-
-        }
-    }
+public class practica_2 {
     public static void main(String[] args) {
-      String []g={"axcvb","ghjk","fghioo"};
-        //Arrays.sort(g);
-        comparar(g);
-        for (int i = 0; i < 3; i++) {
-            System.out.println(g[i]);
+int[]epale= new int[10];
+int[]a= new int[10];
+int[]c= new int[20];
+int aux=0,aux1=0;
+        for (int i = 0; i < epale.length; i++) {
+            epale[i]=i+1;
+            System.out.println(epale[i]);
+
+        }
+        for (int i = 0; i < epale.length/2; i++) {
+            a[aux++]=epale[i];
+           a[aux++]=epale[epale.length-1-i];
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        for (int i = 0; i < a.length; i+=2) {
+            for (int j = 0; j < 2; j++) {
+                c[aux1++]=epale[i+j];
+            }
+            for (int j = 0; j < 2; j++) {
+              c[aux1++]=a[i+j];
+            }
+
+
+        }
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
         }
 
-
-
     }
-    }
-
-
-
-
-
-
-
+}
